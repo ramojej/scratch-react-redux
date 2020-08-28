1ea4b42f6e8c143cc56d3f101eeb485c701085bc
Jejo <jejomardorongon@gmail.com>
2020-08-28 16:20:41 +0800

    Developed foundation, created components, setup react router and navigation

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum,
+        eligendi!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
